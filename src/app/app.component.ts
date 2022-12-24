803978b14197f5bc5facc8b966b869df12684bb5
alexneo68 <tunning_brain@mail.ru>
2022-12-24 12:52:05 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ng-forms-start';
+  @ViewChild('f') signupForm: NgForm;
+  onHandleSubmit() {
+    console.log(this.signupForm);
+  }
 }
+
+
