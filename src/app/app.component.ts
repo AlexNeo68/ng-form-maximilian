a8d98db9c7baae9e2c7eb7592e198f6630f552c9
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2022-12-24 22:17:40 +0300

    finish form template approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  subscriptions: string[] = ['Basic', 'Advanced', 'Pro']
-  selectedSubscription = 'Advanced'
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
   submitted: boolean = false;
+  result = { email: '', subscription: '', password: '' };
+  @ViewChild('signupForm') signupForm: NgForm;
   onHandleSubmit() {
+    this.submitted = true;
+    this.result = { ...this.signupForm.value };
+    this.signupForm.reset();
   }
 }
