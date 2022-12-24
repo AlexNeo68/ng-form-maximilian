dc89f956f332381f79dcec01a86d88eeb63e3780
alexneo68 <tunning_brain@mail.ru>
2022-12-24 19:37:11 +0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,42 +8,9 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AppComponent {
-
-  title = 'ng-forms-start';
-  suggestedUser: string = 'SuperUser';
-  defaultQuestion: string = 'pet';
-  answer: string = '';
-  genders = ['male', 'female'];
-
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro']
+  selectedSubscription = 'Advanced'
   submitted: boolean = false;
-
-  public user: {
-    username: '',
-    email: '',
-    question: '',
-    answer: '',
-    gender: ''
-  }
-
-  @ViewChild('f') signupForm: NgForm;
-
   onHandleSubmit() {
-    this.user = {
-      username: this.signupForm.value.userData.username,
-      email: this.signupForm.value.userData.email,
-      question: this.signupForm.value.secret,
-      answer: this.signupForm.value.answer,
-      gender: this.signupForm.value.gender
-    }
-
-    this.submitted = true;
-    this.signupForm.reset();
   }
-
-  onSuggestedUser() {
-    this.signupForm.form.patchValue({
-      userData: { username: this.suggestedUser },
-    });
-  }
-
 }
