4b4d07532656e3c4227a0bbb25b6ea00c67f8769
alexneo68 <tunning_brain@mail.ru>
2022-12-24 18:31:52 +0300

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,38 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
+
   title = 'ng-forms-start';
   suggestedUser: string = 'SuperUser';
   defaultQuestion: string = 'pet';
   answer: string = '';
-
   genders = ['male', 'female'];
 
+  submitted: boolean = false;
+
+  public user: {
+    username: '',
+    email: '',
+    question: '',
+    answer: '',
+    gender: ''
+  }
+
   @ViewChild('f') signupForm: NgForm;
+
   onHandleSubmit() {
-    console.log(this.signupForm);
+    this.user = {
+      username: this.signupForm.value.userData.username,
+      email: this.signupForm.value.userData.email,
+      question: this.signupForm.value.secret,
+      answer: this.signupForm.value.answer,
+      gender: this.signupForm.value.gender
+    }
+
+    this.submitted = true;
+    this.signupForm.reset();
   }
 
   onSuggestedUser() {
@@ -24,4 +45,5 @@ export class AppComponent {
       userData: { username: this.suggestedUser },
     });
   }
+
 }
